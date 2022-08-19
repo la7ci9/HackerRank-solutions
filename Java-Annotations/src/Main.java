4820f86f42e0c0c54cdcec1bcfb5ccd1235e0831
la7ci9 <akarmiis997@gmail.com>
2022-08-19 12:12:16 +0200

    familymember annotation

--- Java-Annotations/src/Main.java
+++ Java-Annotations/src/Main.java
@@ -10,14 +10,14 @@ import java.util.*;
 }
 
 class FamilyMember {
-    //~~Complete this line~~
+	@FamilyBudget(userRole = "SENIOR", budgetLimit = 100)
     public void seniorMember(int budget, int moneySpend) {
         System.out.println("Senior Member");
         System.out.println("Spend: " + moneySpend);
         System.out.println("Budget Left: " + (budget - moneySpend));
     }
 
-    //~~Complete this line~~
+    @FamilyBudget(userRole = "JUNIOR", budgetLimit = 50)
     public void juniorUser(int budget, int moneySpend) {
         System.out.println("Junior Member");
         System.out.println("Spend: " + moneySpend);
